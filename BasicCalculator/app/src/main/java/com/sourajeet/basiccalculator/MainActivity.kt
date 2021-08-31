c63ef08e189e6fd14dc7944eba5842d48c41d676
sourajeet_dey <write2sjdey@gmail.com>
2021-08-31 18:54:21 +0530

    Made Basic Arithmetic Operations Functional

--- BasicCalculator/app/src/main/java/com/sourajeet/basiccalculator/MainActivity.kt
+++ BasicCalculator/app/src/main/java/com/sourajeet/basiccalculator/MainActivity.kt
@@ -22,8 +22,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var button_subtract : Button
     lateinit var button_multiply : Button
     lateinit var button_divide : Button
-    lateinit var button_modulo : Button
-    lateinit var button_intdivide : Button
+    lateinit var button_percentage : Button
+//    lateinit var button_intdivide : Button
     lateinit var button_equal : Button
     lateinit var button_clear : Button
     lateinit var button_back : Button
@@ -31,9 +31,18 @@ class MainActivity : AppCompatActivity() {
     lateinit var text_answer : TextView
 
     var isAdd : Boolean = false
+    var doAdd : Boolean = false
     var isSubtract : Boolean = false
+    var doSubtract : Boolean = false
     var isMultiply : Boolean = false
+    var doMultiply : Boolean = false
     var isDivide : Boolean = false
+    var doDivide : Boolean = false
+    var isEqual : Boolean = false
+    var isPercentage : Boolean = false
+
+    var input1 : Double = 0.0
+    var input2 : Double = 0.0
 
 
 
@@ -56,8 +65,8 @@ class MainActivity : AppCompatActivity() {
         button_subtract = findViewById(R.id.button_subtract)
         button_multiply = findViewById(R.id.button_multiply)
         button_divide = findViewById(R.id.button_divide)
-        button_modulo = findViewById(R.id.button_modulo)
-        button_intdivide = findViewById(R.id.button_intdivide)
+        button_percentage = findViewById(R.id.button_percentage)
+//        button_intdivide = findViewById(R.id.button_intdivide)
         button_equal = findViewById(R.id.button_equal)
         button_clear = findViewById(R.id.button_clear)
         button_back = findViewById(R.id.button_back)
@@ -93,34 +102,113 @@ class MainActivity : AppCompatActivity() {
         var btn = view as Button
         var text_answer = findViewById<TextView>(R.id.text_answer)
         var text_question = findViewById<TextView>(R.id.text_question)
+        input1 = text_answer.text.toString().toDouble()
         var question : String = text_question.text.toString()+text_answer.text.toString()
-
+        var str1 = text_answer.text.toString()
+        if(isEqual){
+            question = text_answer.text.toString()
+            isEqual = false
+        }
+//        if(str1.isEmpty()){
+//            question = text_answer.text.toString()
+//        }
         when(btn.id){
             button_add.id -> isAdd = true
             button_subtract.id -> isSubtract = true
             button_multiply.id -> isMultiply = true
             button_divide.id -> isDivide = true
+            button_percentage.id -> isPercentage = true
         }
-        if(isAdd){
-            text_question.setText(question+"+")
-            text_answer.setText("")
-            isAdd = false
+        when {
+            isAdd -> {
+                text_question.setText(question+"+")
+                text_answer.setText("")
+                doAdd = true
+                isAdd = false
+            }
+            isSubtract -> {
+                text_question.setText(question+"-")
+                text_answer.setText("")
+                doSubtract = true
+                isSubtract = false
+            }
+            isMultiply -> {
+                text_question.setText(question+"*")
+                text_answer.setText("")
+                doMultiply = true
+                isMultiply = false
+            }
+            isDivide -> {
+                text_question.setText(question+"/")
+                text_answer.setText("")
+                input1 *= 0.01
+                doDivide = true
+                isDivide = false
+            }
+            isPercentage -> {
+                text_question.setText(question+"%")
+                text_answer.setText("")
+                doDivide = true
+                isDivide = false
+            }
+            else -> {
+                text_answer.setText("")
+            }
         }
-        else if(isSubtract){
-            text_question.setText(question+"-")
-            text_answer.setText("")
-            isSubtract = false
+    }
+
+    fun equalTo(view: View){
+        var text_answer = findViewById<TextView>(R.id.text_answer)
+        var text_question = findViewById<TextView>(R.id.text_question)
+        var question : String = text_question.text.toString()+text_answer.text.toString()
+        text_question.setText(question)
+        input2 = text_answer.text.toString().toDouble()
+        if(doAdd){
+            var ans = input1 + input2
+            text_answer.setText(ans.toString())
+            doAdd = false
+            isEqual = true
         }
-        else if(isMultiply){
-            text_question.setText(question+"*")
-            text_answer.setText("")
-            isMultiply = false
+        else if(doSubtract){
+            var ans = input1 - input2
+            text_answer.setText(ans.toString())
+            doSubtract = false
+            isEqual = true
         }
-        else if(isDivide){
-            text_question.setText(question+"/")
-            text_answer.setText("")
-            isDivide = false
+        else if(doMultiply){
+            var ans = input1 * input2
+            text_answer.setText(ans.toString())
+            doMultiply = false
+            isEqual = true
         }
+        else if(doDivide){
+            var ans = input1 / input2
+            text_answer.setText(ans.toString())
+            doDivide = false
+            isEqual = true
+        }
+    }
+
+    fun clearAll(view: View){
+        var text_answer = findViewById<TextView>(R.id.text_answer)
+        var text_question = findViewById<TextView>(R.id.text_question)
+        text_answer.setText("")
+        text_question.setText("")
+        isAdd = false
+        doAdd = false
+        isSubtract = false
+        doSubtract = false
+        isMultiply = false
+        doMultiply = false
+        isDivide = false
+        doDivide = false
+        isEqual = false
+    }
+
+    fun back(view: View){
+        var text_answer = findViewById<TextView>(R.id.text_answer)
+        var str1 : String = text_answer.text.toString()
+        if(str1.isNotEmpty()) text_answer.setText(str1.subSequence(0,str1.length-1))
     }
 
 
